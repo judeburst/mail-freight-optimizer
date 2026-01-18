e90b1930fc527cd646504f8e7aac02a5144bd5e2
judeburst <judeburst1116@gmail.com>
2026-01-18 19:36:49 +0400

    Update pallet_dimensions.py

--- logic/pallet_dimensions.py
+++ logic/pallet_dimensions.py
@@ -1,27 +1,44 @@
-# Example assumptions:
-# - each household = 0.15 lbs of mail
-# - max pallet weight = 2000 lbs
-# - standard pallet footprint: 48x40 inches
-
 HOUSEHOLD_WEIGHT_LB = 0.15
 MAX_PALLET_WEIGHT = 2000
-PALLET_FOOTPRINT = (48, 40)  # inches
+PALLET_DIMENSIONS_IN = (48, 40)
 
-def calculate_pallets(ddus):
+
+def calculate_pallets(destinations):
     """
-    Returns list of pallets with weight, dimensions, and assigned DDUs
+    Handles:
+    - Individual DDU shipments
+    - Grouped SCF shipments (list of DDUs)
     """
+
     pallets = []
-    for ddu in ddus:
-        weight = ddu.households * HOUSEHOLD_WEIGHT_LB
-        num_pallets = max(1, int((weight + MAX_PALLET_WEIGHT - 1) // MAX_PALLET_WEIGHT))  # ceil
 
-        for i in range(num_pallets):
+    for dest in destinations:
+
+        # SCF GROUP
+        if isinstance(dest, list):
+            total_households = sum(d.households for d in dest)
+            weight = total_households * HOUSEHOLD_WEIGHT_LB
+
             pallets.append({
-                "destination": getattr(ddu, "ddu_code", getattr(ddu, "scf_code", "UNKNOWN")),
-                "weight_lbs": min(weight, MAX_PALLET_WEIGHT),
-                "dimensions_in": PALLET_FOOTPRINT,
-                "households": ddu.households,
-                "included_ddus": [getattr(ddu, "ddu_code", "SCF")],
+                "destination": dest[0].scf_code,
+                "type": "SCF",
+                "weight_lbs": round(weight, 2),
+                "dimensions_in": PALLET_DIMENSIONS_IN,
+                "households": total_households,
+                "included_ddus": [d.ddu_code for d in dest]
             })
+
+        # SINGLE DDU
+        else:
+            weight = dest.households * HOUSEHOLD_WEIGHT_LB
+
+            pallets.append({
+                "destination": dest.ddu_code,
+                "type": "DDU",
+                "weight_lbs": round(weight, 2),
+                "dimensions_in": PALLET_DIMENSIONS_IN,
+                "households": dest.households,
+                "included_ddus": [dest.ddu_code]
+            })
+
     return pallets
