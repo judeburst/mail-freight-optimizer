11ae77052914562d263c390e5f205ebea85f5495
judeburst <judeburst1116@gmail.com>
2026-01-18 19:33:00 +0400

    Delete app/models directory

--- app/models/campaign.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from app.db.base import Base
-
-class Campaign(Base):
-    __tablename__ = "campaigns"
-
-    id = Column(Integer, primary_key=True)
-    dealer_name = Column(String, nullable=False)
-    sale_length_days = Column(Integer, nullable=False)
--- app/models/carrier.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from app.db.base import Base
-
-class Carrier(Base):
-    __tablename__ = "carriers"
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String, unique=True)
