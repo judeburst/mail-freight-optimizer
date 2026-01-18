ebec912a9703c9a7a7ef20e38b06f5e04db0ac90
judeburst <judeburst1116@gmail.com>
2026-01-18 19:34:32 +0400

    Update ddu.py

--- models/ddu.py
+++ models/ddu.py
@@ -5,7 +5,7 @@ class DDU(Base):
     __tablename__ = "ddus"
 
     id = Column(Integer, primary_key=True)
-    zip_code = Column(String)
-    ddu_code = Column(String)
-    households = Column(Integer)
-    scf_code = Column(String)
+    zip_code = Column(String, nullable=False)
+    ddu_code = Column(String, nullable=False)
+    scf_code = Column(String, nullable=False)
+    households = Column(Integer, nullable=False)
