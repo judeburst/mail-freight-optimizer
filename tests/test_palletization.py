168655f4c6c24f09e22768f1bbdff0eb45e8e23a
judeburst <judeburst1116@gmail.com>
2026-01-18 19:40:17 +0400

    Update test_palletization.py

--- tests/test_palletization.py
+++ tests/test_palletization.py
@@ -1,10 +1,8 @@
-import unittest
 from types import SimpleNamespace
 from app.logic.pallet_dimensions import calculate_pallets
 
-class TestPalletization(unittest.TestCase):
-    def test_calculate_pallets(self):
-        ddus = [SimpleNamespace(ddu_code="DDU1", scf_code="SCF1", households=5000)]
-        pallets = calculate_pallets(ddus)
-        self.assertEqual(len(pallets), 1)
-        self.assertEqual(pallets[0]["weight_lbs"], 5000 * 0.15)
+def test_calculate_pallets():
+    ddu = SimpleNamespace(ddu_code="DDU1", scf_code="SCF1", households=5000)
+    pallets = calculate_pallets([ddu])
+
+    assert pallets[0]["weight_lbs"] == 750.0
